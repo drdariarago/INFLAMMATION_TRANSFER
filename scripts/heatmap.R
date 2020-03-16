a9d45f19c038e286a565b08a9b8afa5de402e0f3
signe.hansen <signe.hansen@bric.ku.dk>
2020-03-16 16:33:09 +0100

    Plot heatmap of samples from one tissue
    This version implements a number of minimal features for QC.
    1) It plots variance stabilized counts of the genes with the most variance
    2) It displays the annotation of the samples
    3) It clusters genes to detect outliers

--- scripts/heatmap.R
+++ scripts/heatmap.R
@@ -7,30 +7,45 @@ library(DESeq2)
 variance_stabilized_counts <-
   readRDS(here('results/tximeta/vsd.Rdata')) %>%
   DESeqDataSet(se = ., design = ~ tissue + maternal_fetal + exposure + timepoint) %>%
-  vst(object = ., blind = T) %>%
-  assay() 
+  vst(object = ., blind = T)
+
+# Subset matrix with only
+lung_annotated_counts <- 
+  variance_stabilized_counts[, variance_stabilized_counts$tissue == "lung"] 
+
+lung_count_matrix <- 
+  assay(lung_annotated_counts)
+
+lung_annotation <- 
+  colData(lung_annotated_counts) %>% 
+  magrittr::extract(, c(5,7)) %>% 
+  as.data.frame()
 
 # Create matrix with top 1000 variance genes
-top_1000_genes <- 
-  rowVars(variance_stabilized_counts) %>% 
+top_genes <- 
+  lung_count_matrix %>%
+  rowVars() %>% 
   order(decreasing = T) %>% 
-  magrittr::extract(seq_len(1000))
+  magrittr::extract(seq_len(5000))
 
-#Making heatmap of top 1000 variance genes
+# Making heatmap of top 1000 variance genes
 library(RColorBrewer)
 colors <- 
-  colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
+  colorRampPalette(rev(brewer.pal(9, "Blues")) )(255)
 
-top_1000_heatmap <-
-  pheatmap::pheatmap(
-    variance_stabilized_counts[top_1000_genes,] %>% 
-    t(),
-    clustering_distance_rows = "euclidean",
-    clustering_distance_cols = "euclidean",
-    clustering_method = "average",
-    cluster_rows = T,
-    cluster_cols = T,
-    display_numbers = F,
-    cex= 0.5,
-    col = colors
-  )
+
+pheatmap::pheatmap(
+  lung_count_matrix[top_genes,] %>% t(),
+  clustering_distance_rows = "euclidean",
+  clustering_distance_cols = "euclidean",
+  clustering_method = "average",
+  scale = "row",
+  cutree_rows = 10,
+  cluster_rows = T,
+  cluster_cols = T,
+  display_numbers = F,
+  cex= 0.9,
+  show_colnames = F,
+  annotation_row = lung_annotation,
+  col = colors
+)
