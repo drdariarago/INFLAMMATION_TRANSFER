5cd665b31d8d2c03eabae09bb15009d271503d3b
Alfredo Rago <alfredo.rago@gmail.com>
2020-03-17 16:45:27 +0100

    Refactor limma preprocessing for convenience
    Use a few more pipes to reduce the objects created etc

--- scripts/limma.R
+++ scripts/limma.R
@@ -19,30 +19,25 @@ experiment_data <-
       as_tibble %>%
       transmute(group = paste(tissue, timepoint, maternal_fetal, exposure, sep = ":")) %>%
       pull(group)
-  )}
-
-# Remove TiO2 samples
-experiment_data <- 
-  experiment_data[,experiment_data$samples$exposure != "TiO2"] 
-
-experiment_data$samples <- droplevels(experiment_data$samples)
-
-# Perform library normalization
-experiment_data <- 
-  experiment_data %>% 
+  )} %>% 
+  .[,.$samples$exposure != "TiO2"] %>% 
   calcNormFactors(method = "TMM")
 
+# Create convenience variables
+# exposure removes TiO2 factor level
+# tissue2 is necessary to split the dataset
+# timepoint needs to be converted to factor to be used in the contrast design
+
+experiment_data$samples <-
+  experiment_data$samples %>% 
+  mutate(
+    exposure = droplevels(exposure),
+    tissue2 = paste(tissue, maternal_fetal, sep = "_") %>% as.factor(),
+    timepoint = as.factor(experiment_data$samples$timepoint)
+  )
+
 # Create list of datasets (one per tissue)
-experiment_data$samples$tissue2 <-
-  paste(experiment_data$samples$tissue, 
-        experiment_data$samples$maternal_fetal, 
-        sep = "_") %>% 
-  as.factor()
-
 tissue_types = unique(experiment_data$samples$tissue2)
-
-experiment_data$samples$timepoint <-
-  as.factor(experiment_data$samples$timepoint)
 
 experiment_list <-
   lapply(
