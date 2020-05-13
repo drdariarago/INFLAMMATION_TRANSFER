b69eddf9ea7b5e339db13dd7b80f26b06e19aba4
Alfredo Rago <alfredo.rago@gmail.com>
2020-05-13 11:03:22 +0200

    BUGFIX: include changes to enrichment run rule
    This version includes the changes required to allow the rule to run as part of the pipeline from commit 5d22cdd

--- scripts/gost_enrichment_run.R
+++ scripts/gost_enrichment_run.R
@@ -2,27 +2,22 @@ library(tidyverse)
 library(magrittr)
 library(gprofiler2)
 
-# Import background set of genes expressed in each tissue
-tissue_bg <-
-  read_rds(path = snakemake@input[["expressed_genes"]])
-
 # Import list of ranked gene names
 ranked_gene_list <- 
   read_rds(path = snakemake@input[["ranked_genes"]])
 
 # Run GO enrichment
 gost_result_list <-
-  map2(
+  map(
     .x = ranked_gene_list,
-    .y = tissue_bg,
     .f = ~ gprofiler2::gost(
       query = .x,
       ordered_query = TRUE, 
-      multi_query = TRUE,
+      multi_query = FALSE,
       organism = "mmusculus",
       sources = c("GO"), 
       domain_scope = "custom", 
-      custom_bg = .y,
+      custom_bg = .x,
       correction_method = "gSCS",
       user_threshold = snakemake@params[["max_fdr"]],
       significant = TRUE,
