d811148947f899d2150baa9ac31c264b1eb8d467
Alfredo Rago <alfredo.rago@gmail.com>
2020-09-30 11:49:23 +0200

    Working interaction plot prototype
    Not snakemake integrated
    Filters only DE ligands and expressed receptors
    Plots everything according to logFC and q_values of logFC.
    I split the dataset via map in order to avoid multiple mapping (receptor/receptor, ligand/ligand)
    The plots are then reconnected via patchwork

--- scripts/receptor_ligand_filter.R
+++ scripts/receptor_ligand_filter.R
@@ -5,11 +5,14 @@
 library(tidyverse)
 library(magrittr)
 library(ggplot2)
+library(patchwork)
 
 receptor_ligand_pairs <-
   read_tsv(file = "results/match_orthologs/human_mouse_ligands_receptors.txt") %>% 
   select(matches("ensembl.*mouse")) %>% 
-  set_names(c("ligand", "receptor"))
+  set_names(c("ligand", "receptor")) %>% 
+  filter(!is.na(receptor), !is.na(ligand)) %>% 
+  distinct()
 
 fold_change_summary <-
   c("fetal_liver", "maternal_liver", "maternal_lung", "placentas") %>% 
@@ -18,7 +21,9 @@ fold_change_summary <-
   map_dfr(read_rds, .id = "tissue") %>% 
   mutate(
     tissue = as.factor(tissue)
-  )
+  ) %>% # Remove chorion/decidua contrasts
+  filter(is.na(maternal) | maternal == "shared") %>% 
+  select(-maternal)
 
 # Define which tissues we scan for ligands (messenger tissues) and receptors (target tissues)
 # Also define other global criteria
@@ -29,70 +34,82 @@ timepoints <- paste("timepoint", c(2,5), sep = "")
 log_fc_threshold <- 0.5
 q_value_threshold <-0.05
 
-# Subset only receptors that are expressed in the target tissues and the ligands that target them
+# Subset only receptors that are expressed (more than 8 counts baseline in at least one timepoint of interest) 
+# in the target tissues and the ligands that target them
 
 expressed_receptors <-
   fold_change_summary %>% 
-  filter(tissue %in% target_tissues) %>% 
+  filter(tissue %in% target_tissues) %>%
+  filter(timepoint %in% timepoints) %>% 
+  filter(exposure == "baseline", logFC > 3) %>% 
   select(expressed_receptor = ensembl_gene_id) %>% 
-  distinct() %>% 
-  inner_join(x = ., y = receptor_ligand_pairs, by = c("expressed_receptor" = "receptor"))
+  distinct() 
 
 # Subset ligands differentially expressed in messenger tissues at 2 and 5 hours (logFC > 0.5, q-val < 0.05)
 # Remove ligands whose receptors are not expressed in target tissues
 
-expressed_receptor_ligand_pairs <-
+expressed_ligands <-
   fold_change_summary %>% 
-  filter( tissue %in% messenger_tissues) %>% 
-  filter( timepoint %in% timepoints ) %>% 
-  filter( exposure == "response") %>%   
-  filter( q_value < q_value_threshold, abs(logFC) > log_fc_threshold ) %>% 
-  select( differentially_expressed_ligand = ensembl_gene_id ) %>% 
-  inner_join( x = ., y = expressed_receptors, by = c("differentially_expressed_ligand" = "ligand")) %>% 
-  unique()
+  filter(tissue %in% messenger_tissues) %>% 
+  filter(timepoint %in% timepoints ) %>% 
+  filter(exposure == "response") %>%   
+  filter(q_value < q_value_threshold, abs(logFC) > log_fc_threshold ) %>% 
+  select(expressed_ligand = ensembl_gene_id ) %>% 
+  unique() 
 
-# Annotated subset of DE ligand - expressed receptor pairs with ligand expression data
+# Merge expressed receptor and differentially expressed ligands, discard interactions without a partner
 
-annotated_curated_ligands <-
+expressed_receptor_ligand_pairs <-
+  receptor_ligand_pairs %>% 
+  filter(
+    ligand %in% expressed_ligands$expressed_ligand,
+    receptor %in% expressed_receptors$expressed_receptor
+  )
+  
+# Extract annotation for all genes in the final ligand-receptor pairs
+
+ligand_receptor_annotation <-
   fold_change_summary %>% 
   filter(
-    tissue %in% messenger_tissues, 
     timepoint %in% timepoints
   ) %>% 
-  select( ensembl_gene_id, tissue, timepoint, exposure, logFC ) %>% 
-  left_join( 
-    x = expressed_receptor_ligand_pairs, y = ., 
-    by = c("differentially_expressed_ligand" = "ensembl_gene_id") 
-  ) %>% 
-  select( - expressed_receptor) %>%
-  distinct() %>% 
-  pivot_wider( names_from = exposure, values_from = logFC) %>% 
-  filter(abs(response) > log_fc_threshold) %>% 
-  group_by( tissue, timepoint) %>% 
+  select( ensembl_gene_id, tissue, timepoint, exposure, log_fc = logFC, q_value ) %>% 
+  pivot_wider( names_from = exposure, values_from = c(log_fc, q_value) )
+
+# Annotate receptor-ligand pairs in long format
+annotated_receptor_ligand_pairs <-
+  expressed_receptor_ligand_pairs %>% 
   mutate(
-    ligand_rank = rank(- response),
-    ligand_fc = response
+    pair_id = paste(ligand, receptor, sep = "_")
+  ) %>% 
+  pivot_longer(
+    cols = c("ligand", "receptor"), 
+    names_to = "gene_class", 
+    values_to = "ensembl_gene_id"
+  ) %>% 
+  left_join(
+    x = ., y = ligand_receptor_annotation,
+    by = "ensembl_gene_id"
+  ) %>% # Filter only receptor data from target tissues and ligand data from messenger tissues
+  filter(gene_class == "receptor" & tissue %in% target_tissues | gene_class == "ligand" & tissue %in% messenger_tissues) %>% 
+  filter( # Filter out receptors with no expression (baseline q > 0.05) and ligands with no response (baseline q > 0.05 or response fc < 0.5)
+    gene_class == "receptor" & q_value_baseline < q_value_threshold | gene_class == "ligand" & q_value_response < q_value_threshold & log_fc_response < log_fc_threshold
   )
 
-# Rank ligands at time 2 by logFC
-# Rank receptors by mean logFC of their ligands
-# Annotate average expression of receptors and ligands
-# Group ligands by receptor
-# plot
-
-ranked_receptor_ligand_pairs <-
-  annotated_curated_ligands %>% 
-  inner_join(x = expressed_receptor_ligand_pairs, y = ., by = "differentially_expressed_ligand") %>% 
-  as_tibble() %>% 
-  select( - baseline, - response) %>% 
-  group_by(tissue, timepoint, expressed_receptor) %>% 
-  mutate(
-    receptor_rank = mean(ligand_rank),
-    receptor_fc = mean(ligand_fc),
-    n_ligands = n_distinct(differentially_expressed_ligand)
-    ) %>% 
-  arrange(timepoint, tissue, - receptor_fc) %>% 
-  ungroup()
+# Annotate if the ligands have an expressed interacting receptor and vice versa or if the ligand/receptor is orphan
+annotated_receptor_ligand_pairs_orphaned <-
+  annotated_receptor_ligand_pairs %>% 
+  select(pair_id, gene_class, timepoint) %>% 
+  distinct() %>% 
+  group_by( pair_id, timepoint ) %>% 
+  summarize(
+    paired = ifelse( n() > 1, "paired", "orphan") %>% as.factor()
+  ) %>% 
+  left_join(
+    x = annotated_receptor_ligand_pairs,
+    y = ., 
+    by = c('pair_id', 'timepoint')
+  )
 
 # Import gene names
 gene_names <- 
@@ -100,72 +117,48 @@ gene_names <-
   select(-description)
 
 named_receptor_ligand_pairs <- 
-  ranked_receptor_ligand_pairs %>% 
   right_join(
-    x = gene_names, y = ., by = c("ensembl_gene_id" = "differentially_expressed_ligand")
-  ) %>% 
-  rename(ligand_ensembl_id = ensembl_gene_id, ligand_name = mgi_symbol) %>% 
-  right_join(
-    x = gene_names, y = ., by = c("ensembl_gene_id" = "expressed_receptor")
-  ) %>% 
-  rename(receptor_ensembl_id = ensembl_gene_id, receptor_name = mgi_symbol) %>% 
-  select(
-    timepoint, tissue, 
-    name_receptor = receptor_name, rank_receptor = receptor_rank, fc_receptor = receptor_fc, 
-    n_ligands, name_ligand = ligand_name, rank_ligand = ligand_rank, fc_ligand = ligand_fc
+    x = gene_names,
+    y = annotated_receptor_ligand_pairs_orphaned,
+    by = "ensembl_gene_id"
   )
-  
-# Create human readable output table
-named_receptor_ligand_pairs %>% 
-  group_by(name_receptor) %>% 
-  summarise(
-    avg_receptor_rank = mean(rank_receptor)
-  ) %>% 
-  ungroup() %>% 
-  transmute(
-    name_receptor = name_receptor,
-    receptor_avg_rank = rank(avg_receptor_rank)
-  ) %>% 
-  left_join(x = ., y = named_receptor_ligand_pairs, by = "name_receptor") %>% 
-  arrange(receptor_avg_rank, name_ligand, timepoint, tissue) %>% 
-  write_csv(here::here("results/receptor_ligand_filter/receptor_ligand_fc_table.csv"))
+
 
 # Graph interactions
+# Create 4 plots: lung to placenta, lung to liver, liver to placenta, liver to liver
+# use purrr map wiht 2 lists (messengers, targets) to subset and iterate ggplot
 
-long_paired_data <-
-  named_receptor_ligand_pairs %>% 
-  select(- n_ligands) %>% 
-  mutate(
-    pairs = paste(name_ligand, name_receptor, sep = "_")
-  ) %>% 
-  pivot_longer(
-    cols = contains("_"),
-    names_to = c(".value", "ligand"),
-    names_sep = "_",
-    values_drop_na = FALSE
-  ) %>% 
-  mutate(
-    ligand = factor(x = ligand, levels = c("ligand", "receptor"))
+interaction_plots <-
+  crossing(
+  messenger_tissues, target_tissues
+) %>% 
+  t() %>% 
+  as.data.frame() %>% 
+  as.list() %>% 
+  map(.x = ., .f = ~ as.vector(.x)) %>% 
+  map(
+  .x = .,
+  .f = ~ filter( named_receptor_ligand_pairs, tissue %in% .x, paired == "paired")
+) %>% 
+  map(
+    .x = .,
+    .f = ~ ggplot(
+      data = .x,
+      aes(
+        x = gene_class, y = log_fc_response, col = tissue, alpha = - log10(q_value_response)
+      )
+    ) + 
+      geom_point() +
+      geom_line(aes(group = pair_id)) +
+      facet_wrap(~ timepoint) +
+      scale_y_continuous(name = "log Fold Change") +
+      scale_x_discrete(name = NULL)  +
+      scale_alpha_continuous(name = "-log10 qvalue", breaks = c(2,5,10,15))
   )
-  
-long_paired_data %>% 
-  ggplot(
-    aes(x = ligand, y = fc, group = pairs)
-  ) + 
-  geom_point(alpha = 0.1) +
-  geom_line(alpha = 0.1) +
-  facet_grid(tissue ~ timepoint) +
-  scale_y_continuous(name = "log Fold Change of Ligands") +
-  scale_x_discrete(name = NULL) +
-  ggtitle(label = "log Fold Change of ligands in messenger tissues and average of the ligands\nthat bind to each receptor in target tissues")
 
-ggsave(filename = here::here("results/receptor_ligand_filter/receptor_plot.pdf"))
+interaction_plots[[1]] + interaction_plots[[2]] + interaction_plots[[3]] + interaction_plots[[4]]
+
+ggsave(filename = here::here("results/receptor_ligand_filter/receptor_plot.pdf"), 
+       width = 297, height = 210, units = "mm")
 
 # Add labels to top ligands/receptors (gene symbols)
-# Annotate if receptors are placenta or liver
-
-# MA plot of curated ligands by tissue and time
-annotated_curated_ligands %>% 
-  ggplot( aes( x = baseline, y = response) )+
-  geom_point() +
-  facet_grid( tissue ~ timepoint )
