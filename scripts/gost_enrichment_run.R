73493873325a0a04757a57d5b3f4d8dfd196579f
Alfredo Rago <alfredo.rago@gmail.com>
2021-11-04 09:47:08 +0100

    More stringent GO analysis
    Filters out genes with less than 0 baseline expression (line 22)
    Uses all genes expressed in the tissue across all timepoints as background
    Correction for q-value: before it was signed so wrong sorting in the downregulated list

--- scripts/gost_enrichment_run.R
+++ scripts/gost_enrichment_run.R
@@ -12,34 +12,44 @@ limma_results <-
   # here::here("results/limma_compile_results/limma_results_no_maternal_contrasts.csv") %>%
   snakemake@input[['limma_results']] %>%
   read_csv() %>% 
-  filter(tissue == model) %>% 
-  filter(exposure == "response")
+  filter(tissue == model) 
 
 # Set background gene expression as all responder genes across all timepoints
 background_genes <-
   limma_results %>% 
-  pull(ensembl_gene_id) %>% 
+  filter(
+    exposure == "baseline",
+    logFC > 0
+  ) %>% 
+  dplyr::pull(ensembl_gene_id) %>% 
   unique()
 
 # Filter log fold change above threshold in the right direction
 filtered_results <-
+  limma_results %>% 
+  filter(
+    exposure == "response",
+    # Ensure that we select only genes present in background
+    ensembl_gene_id %in% background_genes
+  ) %>% 
+  arrange( log10(q_value) )
+
+directional_results <-
   if (direction == "upregulated") {
-    limma_results %>% 
-      arrange( -log10(q_value) * -sign(logFC) )
+    filtered_results[filtered_results$logFC > 0,]
   } else if (direction == "downregulated") {
-    limma_results %>% 
-      arrange( -log10(q_value) * sign(logFC) )
+    filtered_results[filtered_results$logFC < 0,]
   } else {
     print("Direction needs to be either 'downregulated' or 'upregulated'")
   }
 
 # Convert to list of ranked gene IDs
 ranked_gene_list<-
-  filtered_results$timepoint %>% 
+  directional_results$timepoint %>% 
   unique %>% 
   purrr::set_names() %>% 
   map(
-    .f = ~ filtered_results[filtered_results$timepoint == .x, "ensembl_gene_id"]
+    .f = ~ directional_results[directional_results$timepoint == .x, "ensembl_gene_id"]
   ) %>% 
   map(.f = pull)
 
